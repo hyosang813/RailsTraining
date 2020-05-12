a265b3f662637d397a5422d31e99602c53b1a9ad
kosou.tei <kosou.tei@JMPC-0103.local>
2020-05-12 20:29:58 +0900

    第4章 Rails風味のRuby - ベースタイトル設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # デフォルトページタイトルを設定
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
