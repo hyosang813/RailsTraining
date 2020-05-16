7528bcacc2946899c9fc017f84b479a6684b6c3e
kosou.tei <kosou.tei@JMPC-0103.local>
2020-05-16 20:13:12 +0900

    第7章 ユーザ登録 - 他全部

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 # RailsTutorial用だからAPI用とは違うけど同じ名前
 class UsersController < ApplicationController
-    def new
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+       flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # 成功したらユーザページへ
+    else
+      render 'new' # 失敗したらnewアクションをレンダリング
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
     end
 end
