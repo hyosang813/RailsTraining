10e37b6452faea308fe3bfc63b25cf74e2cb09a6
kosou.tei <kosou.tei@JMPC-0103.local>
2020-05-17 18:11:34 +0900

    共通 - 例外抑制をtestに波及

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with invalid information" do
-    Mongoid.raise_not_found_error = false # なぜかConfigファイルのこの設定値を読んでくれないから個別で設定
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
